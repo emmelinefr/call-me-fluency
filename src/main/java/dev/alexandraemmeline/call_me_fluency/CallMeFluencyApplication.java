c3cfcffeaf70ae0ef0f15c64cd8a2621b664f832
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2026-06-23 13:34:08 -0300

    initial commit

---
+++ src/main/java/dev/alexandraemmeline/call_me_fluency/CallMeFluencyApplication.java
@@ -0,0 +1,13 @@
+package dev.alexandraemmeline.call_me_fluency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallMeFluencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallMeFluencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alexandraemmeline/call_me_fluency/CallMeFluencyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexandraemmeline.call_me_fluency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallMeFluencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
