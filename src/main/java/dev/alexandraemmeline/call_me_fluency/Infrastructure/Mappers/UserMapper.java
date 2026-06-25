aa504f0303c0960058f3527be0169a44d022bd74
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2026-06-25 16:30:24 -0300

    feat: introduce UserMapper for cross-layer object mapping (DTO, domain, entity)

---
+++ src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/Mappers/UserMapper.java
@@ -0,0 +1,22 @@
+package dev.alexandraemmeline.call_me_fluency.Infrastructure.Mappers;
+
+import dev.alexandraemmeline.call_me_fluency.Core.Domains.UserDomain;
+import dev.alexandraemmeline.call_me_fluency.Infrastructure.DTOs.CreateUserRequest;
+import dev.alexandraemmeline.call_me_fluency.Infrastructure.DTOs.UserResponse;
+import dev.alexandraemmeline.call_me_fluency.Infrastructure.Persistence.UserEntity;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserDomain toDomain(UserEntity userEntity);
+
+    UserEntity toEntity(UserDomain userDomain);
+
+    UserResponse toResponse(UserDomain userDomain);
+
+    List<UserResponse> toUserResponseList(List<UserEntity> userEntityList);
+
+}
