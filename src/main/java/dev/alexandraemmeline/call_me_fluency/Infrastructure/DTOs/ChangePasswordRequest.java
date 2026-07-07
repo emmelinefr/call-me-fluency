646caf97f731a561e554d16b9ebdc5529a086b1f
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2026-07-07 16:42:27 -0300

    refactor: improve change password request validation

--- src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/DTOs/ChangePasswordRequest.java
+++ src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/DTOs/ChangePasswordRequest.java
@@ -1,12 +1,16 @@
 package dev.alexandraemmeline.call_me_fluency.Infrastructure.DTOs;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ChangePasswordRequest(
 
         @NotBlank
-        @Size(min = 8)
+        @Email
+        String email,
+
+        @NotBlank
         String currentPassword,
 
         @NotBlank
