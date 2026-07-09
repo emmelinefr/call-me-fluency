d69e1889189ceefa29292004f1ebf00cd4c3bb4a
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2026-07-09 17:01:06 -0300

    feat: add UserDetails adapter to decouple UserEntity from Spring Security

---
+++ src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/Adapters/UserDetailsAdapter.java
@@ -0,0 +1,56 @@
+package dev.alexandraemmeline.call_me_fluency.Infrastructure.Adapters;
+
+import dev.alexandraemmeline.call_me_fluency.Infrastructure.Persistence.UserEntity;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsAdapter implements UserDetails {
+
+    private final UserEntity userEntity;
+
+    public UserDetailsAdapter(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return userEntity.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .toList();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return userEntity.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return userEntity.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
