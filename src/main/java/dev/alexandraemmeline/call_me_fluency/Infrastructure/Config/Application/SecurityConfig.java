47401110ce6c441516e3f0d39a05394ab7954ace
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2026-07-28 12:16:51 -0300

    fix: correct permitted endpoint configuration in SecurityConfig

--- src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/Config/Application/SecurityConfig.java
+++ src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/Config/Application/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 )
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST,"v1/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
