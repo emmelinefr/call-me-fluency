e678b1b3ff259afd9b68b9e35082579f4b085c56
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2026-09-14 15:40:55 -0300

    feat(persistence): add embeddable practice day entity

---
+++ src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/Persistence/PracticeDayEntity.java
@@ -0,0 +1,28 @@
+package dev.alexandraemmeline.call_me_fluency.Infrastructure.Persistence;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PracticeDayEntity {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "day_of_week", nullable = false)
+    private DayOfWeek dayOfWeek;
+
+    @Column(nullable = false)
+    private LocalTime time;
+
+    @Column(nullable = false)
+    private int duration;
+
+}
