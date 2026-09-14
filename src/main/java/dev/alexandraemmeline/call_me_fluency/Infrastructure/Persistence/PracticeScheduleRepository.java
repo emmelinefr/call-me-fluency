1458b04a77ee4d6f996c432fdf312454bd11717e
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2026-09-14 16:29:28 -0300

    feat(persistence): add practice schedule repository

---
+++ src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/Persistence/PracticeScheduleRepository.java
@@ -0,0 +1,6 @@
+package dev.alexandraemmeline.call_me_fluency.Infrastructure.Persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PracticeScheduleRepository extends JpaRepository<PracticeScheduleEntity, Long> {
+}
