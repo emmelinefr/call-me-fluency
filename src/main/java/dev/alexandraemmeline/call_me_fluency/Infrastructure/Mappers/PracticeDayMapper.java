da689533c490f8097df9f39d2c4cb3ab0e9338c8
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2026-09-18 14:58:56 -0300

    feat(practice-day): add request to domain mapping

--- src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/Mappers/PracticeDayMapper.java
+++ src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/Mappers/PracticeDayMapper.java
@@ -1,12 +1,15 @@
 package dev.alexandraemmeline.call_me_fluency.Infrastructure.Mappers;
 
 import dev.alexandraemmeline.call_me_fluency.Core.Domains.PracticeDayDomain;
+import dev.alexandraemmeline.call_me_fluency.Infrastructure.DTOs.PracticeSchedule.CreatePracticeDayRequest;
 import dev.alexandraemmeline.call_me_fluency.Infrastructure.Persistence.PracticeSchedule.PracticeDayEntity;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface PracticeDayMapper {
+
+    PracticeDayDomain toDomain(CreatePracticeDayRequest createPracticeDayRequest);
 
     PracticeDayEntity toEntity(PracticeDayDomain practiceDayDomain);
 
