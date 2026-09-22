050a3de7434e933b0c474e15d07ffcb5e7429023
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2026-09-21 21:22:39 -0300

    feat(practice-schedule): map practice days from entity to domain

--- src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/Mappers/PracticeScheduleMapper.java
+++ src/main/java/dev/alexandraemmeline/call_me_fluency/Infrastructure/Mappers/PracticeScheduleMapper.java
@@ -1,13 +1,18 @@
 package dev.alexandraemmeline.call_me_fluency.Infrastructure.Mappers;
 
+import dev.alexandraemmeline.call_me_fluency.Core.Domains.PracticeDayDomain;
 import dev.alexandraemmeline.call_me_fluency.Core.Domains.PracticeScheduleDomain;
+import dev.alexandraemmeline.call_me_fluency.Core.Domains.UserDomain;
 import dev.alexandraemmeline.call_me_fluency.Infrastructure.DTOs.PracticeSchedule.CreatePracticeScheduleRequest;
 import dev.alexandraemmeline.call_me_fluency.Infrastructure.DTOs.PracticeSchedule.PracticeScheduleResponse;
 import dev.alexandraemmeline.call_me_fluency.Infrastructure.DTOs.PracticeSchedule.UpdatePracticeScheduleRequest;
+import dev.alexandraemmeline.call_me_fluency.Infrastructure.Persistence.PracticeSchedule.PracticeDayEntity;
 import dev.alexandraemmeline.call_me_fluency.Infrastructure.Persistence.PracticeSchedule.PracticeScheduleEntity;
-import org.mapstruct.InheritInverseConfiguration;
+import dev.alexandraemmeline.call_me_fluency.Infrastructure.Persistence.User.UserEntity;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(
         componentModel = "spring",
@@ -27,9 +32,17 @@ public interface PracticeScheduleMapper {
 
 
     @Mapping(source = "userDomain", target = "user")
-    PracticeScheduleEntity toEntity(PracticeScheduleDomain practiceScheduleDomain);
+    PracticeScheduleEntity toEntity(PracticeScheduleDomain practiceScheduleDomain); //ok
 
-    @InheritInverseConfiguration
+    @Mapping(source = "user", target = "userDomain")
+    @Mapping(target = "practiceDays", ignore = true)
     PracticeScheduleDomain toDomain(PracticeScheduleEntity practiceScheduleEntity);
+
+    @AfterMapping
+    default void mapPracticeDays(PracticeScheduleEntity entity, @MappingTarget PracticeScheduleDomain domain) {
+        entity.getPracticeDays()
+                .forEach(dayEntity -> domain.addPracticeDay(toDomain(dayEntity)));
+    }
+    PracticeDayDomain toDomain(PracticeDayEntity practiceDayEntity);
 
 }
